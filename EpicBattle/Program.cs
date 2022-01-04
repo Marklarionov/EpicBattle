ca4ba252df1bf390f28b581aa6409b6b1d703eb4
Opilane TTHK <opilane@tthk.ee>
2022-01-04 09:46:37 +0200

    Add project files.

---
+++ EpicBattle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EpicBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] heroes = {"Harry Potter", "SuperMan", "Luke Skywalker", "Lara Croft"};
+            string[] villains = {"Voldemort", "Joker", "Venom", "Darth Vader", "Cruella"};
+
+
+
+            string randomHero = GetRandomCharacter(heroes);
+            string randomvillain = GetRandomCharacter(villains);
+            Console.WriteLine($"Your random  hero is: {randomHero}");
+            Console.WriteLine($"Your random  villains is: {randomvillain}");
+        }
+
+        public static string GetRandomCharacter(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomCharacter = someArray[randomIndex];
+            return randomCharacter;
+        }
+    }
+}
